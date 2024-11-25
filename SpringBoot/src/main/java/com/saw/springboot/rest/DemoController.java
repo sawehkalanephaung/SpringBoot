ea719ccf8f166441cb66ae94dfff39a0c27348d7
saw <saw@saws-MacBook-Pro.local>
2024-11-25 14:52:53 +0700

    add example properties and expose in in rest controller
    Access endpoint
    lcoalhost:3000/teaminfo

--- SpringBoot/src/main/java/com/saw/springboot/rest/DemoController.java
+++ SpringBoot/src/main/java/com/saw/springboot/rest/DemoController.java
@@ -1,5 +1,7 @@
 package com.saw.springboot.rest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +24,19 @@ public class DemoController {
     public String fortune() {
         return "Today is your very lucky day!";
     }
+
+
+    // inject the properties for the coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+        private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String teamInfo(){
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
 }
